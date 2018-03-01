24e2e0038038430c666feb2c1d210da1f2d22dd5
John Vasquez <jvasquez864@gmail.com>
2018-02-28 23:46:07 -0500

    Refactor MainActivity to be the source of the Material Design Tabbed Activity, and moved Note into its own fragment/fragment layout

--- app/src/main/java/com/jayd/swissarmyknife/activities/MainActivity.java
+++ app/src/main/java/com/jayd/swissarmyknife/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.jayd.swissarmyknife.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 import com.jayd.swissarmyknife.MyApplication;
 import com.jayd.swissarmyknife.R;
@@ -15,19 +19,30 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 
     private NoteDBHelper noteDBHelper;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noteDBHelper = ((MyApplication) getApplication()).getNoteDBHelper();
 
-        final EditText editText = findViewById(R.id.editText);
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(l -> {
-            Note note = new Note(UUID.randomUUID().toString(), editText.getText().toString());
-            noteDBHelper.insert(note);
-            editText.setText("");
-        });
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        //Initialize ViewPager fragments
+
+//        noteDBHelper = ((MyApplication) getApplication()).getNoteDBHelper();
+
+//        final EditText editText = findViewById(R.id.editText);
+//        final Button button = findViewById(R.id.button);
+//        button.setOnClickListener(l -> {
+//            Note note = new Note(UUID.randomUUID().toString(), editText.getText().toString());
+//            noteDBHelper.insert(note);
+//            editText.setText("");
+//        });
     }
 }
