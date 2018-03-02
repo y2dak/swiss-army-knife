ddcc07b6e11b3baad5740dc0b2dd7786b035cc37
John Vasquez <jvasquez864@gmail.com>
2018-03-01 23:10:41 -0500

    Set up view pager and Calendar, Notes, and Expenses fragments.  Also setting up drawables to change icons of selected/unselected tabs
    Fix changes based on yatin reviews

--- app/src/main/java/com/jayd/swissarmyknife/activities/MainActivity.java
+++ app/src/main/java/com/jayd/swissarmyknife/activities/MainActivity.java
@@ -2,19 +2,19 @@ package com.jayd.swissarmyknife.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
-import com.jayd.swissarmyknife.MyApplication;
 import com.jayd.swissarmyknife.R;
-import com.jayd.swissarmyknife.models.Note;
+import com.jayd.swissarmyknife.fragments.CalendarFragment;
+import com.jayd.swissarmyknife.fragments.ExpenseFragment;
+import com.jayd.swissarmyknife.fragments.NoteFragment;
 import com.jayd.swissarmyknife.room.note.NoteDBHelper;
-
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +22,24 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
 
+    //Tab numbers
+    private static final int CALENDAR = 0;
+    private static final int NOTES = 1;
+    private static final int EXPENSES = 2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        tabLayout = (TabLayout)findViewById(R.id.tabs);
-        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //Initialize ViewPager fragments
+        initializeViewPager();
 
+        //TODO: Move commented code into Note fragment
 //        noteDBHelper = ((MyApplication) getApplication()).getNoteDBHelper();
 
 //        final EditText editText = findViewById(R.id.editText);
@@ -44,5 +49,51 @@ public class MainActivity extends AppCompatActivity {
 //            noteDBHelper.insert(note);
 //            editText.setText("");
 //        });
+    }
+
+    private void initializeViewPager(){
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //Set tab icons
+        try{
+            tabLayout.getTabAt(CALENDAR).setIcon(R.drawable.ic_calendar_icon);
+            tabLayout.getTabAt(NOTES).setIcon(R.drawable.ic_note_icon);
+            tabLayout.getTabAt(EXPENSES).setIcon(R.drawable.ic_expenses_icon);
+        } catch(NullPointerException e){
+            Toast.makeText(this, R.string.tab_icons_not_found, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+        private static final int TAB_COUNT = 3;
+
+        public ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case CALENDAR:
+                    return CalendarFragment.newInstance();
+                case NOTES:
+                    return NoteFragment.newInstance();
+                case EXPENSES:
+                    return ExpenseFragment.newInstance();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
     }
 }
